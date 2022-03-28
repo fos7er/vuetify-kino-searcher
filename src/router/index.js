f4564fd5da346e931c08c4078ed98c45557ca14d
Stozen-spb <2012stalker@gmail.com>
2022-03-28 22:26:25 +0300

    added navigation guards

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import routes from './routes'
+import store from '../store'
 
 Vue.use(VueRouter)
 
@@ -8,6 +9,21 @@ const router = new VueRouter({
   base: '/',
   mode: 'history',
   routes,
+})
+
+const auth = {
+  isLoggedIn() {
+    return false
+  },
+}
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !auth.isLoggedIn()) {
+    next({
+      path: '/login',
+      query: { redirect: to.fullPath },
+    })
+  } else next()
 })
 
 export default router
