00f6c3d1c7ea849e05832d6500d831dd6988cbc5
Alexandr Kurkin <2012stalker@gmail.com>
2022-12-13 21:31:10 +0300

    fixed locales

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -4,16 +4,18 @@ import VueI18n from 'vue-i18n'
 Vue.use(VueI18n)
 
 function loadLocaleMessages () {
-  const locales = require.context('@/locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
-  const messages = {}
-  locales.keys().forEach((key) => {
+  const locales = require.context('../locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
+  const messages = {
+    ru: {},
+    en: {}
+  }
+  locales.keys().forEach(key => {
     const matched = key.match(/([A-Za-z0-9-_]+)\./i)
     if (matched && matched.length > 1) {
       const locale = matched[1]
-      messages[locale] = locales(key)
+      messages[locale] = Object.assign(messages[locale], locales(key))
     }
   })
-  console.log(messages)
   return messages
 }
 
