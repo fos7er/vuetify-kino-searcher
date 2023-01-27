6c0b3da1f7211b759e2a37ddb718438f3f9ff118
Alexandr Kurkin <2012stalker@gmail.com>
2023-01-28 02:47:11 +0300

    fixed defaultLang

--- src/utils/defaultLang.js
+++ src/utils/defaultLang.js
@@ -2,7 +2,7 @@ import languages from '@/locales/languages'
 import { LocalStorage } from '@/utils/WebStorage'
 
 export default function getDefaultLanguage () {
-  if (LocalStorage.settings.lang) {
+  if (LocalStorage.settings?.lang) {
     return LocalStorage.settings.lang
   }
   const browserLang = navigator.language || navigator.userLanguage
