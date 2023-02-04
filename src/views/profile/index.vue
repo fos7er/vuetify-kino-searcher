3573f9e251a25b9aad679ae82ccbf1b2fe1e795e
Alexandr Kurkin <2012stalker@gmail.com>
2023-02-04 10:54:42 +0300

    profile preview

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,5 +1,8 @@
 <template>
   <v-container>
+    <v-row justify="center">
+      <v-col> <h1 class="text-center">Тут пока ничего нет 😅</h1></v-col>
+    </v-row>
     <router-view></router-view>
   </v-container>
 </template>
